cb1a7b71ad90346726772b2cf28855b23a537de6
EstoesMoises <contactomoises1@gmail.com>
2025-03-11 12:41:07 +0000

    Added config.d.ts to the frontend plugin

---
+++ plugins/stack-overflow-teams/config.d.ts
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export interface Config {
+    /**
+     * Configuration options for the stack overflow for teams frontend plugin. 
+     * 
+     * This configuration is shared with the backstage-plugin-stack-overflow-teams-backend and backstage-stack-overflow-teams-collator
+     */
+    stackoverflow: {
+      /**
+       * The base url of the Stack Overflow API used for the plugin
+       */
+      baseUrl: string;
+  
+      /**
+       * The API Access Token to authenticate to Stack Overflow API Version 3
+       * @visibility secret
+       */
+      apiAccessToken: string;
+  
+      /**
+       * The name of the team for a Stack Overflow for Teams account
+       */
+      teamName?: string;
+
+      /**
+       * Client Id for the OAuth Application, required to use the Stack Overflow for Teams Hub and write actions.
+       */
+      clientId: number;
+
+      /**
+       * RedirectUri for the OAuth Application, required to use the Stack Overflow for Teams Hub and write actions.
+       * 
+       * This should be your Backstage application domain ending in the plugin's <StackOverflowTeamsPage /> route
+       */
+      redirectUri: string;
+  
+      /**
+       * Type representing the request parameters.
+       */
+      requestParams?: {
+        [key: string]: string | string[] | number;
+      };
+      /**
+       * Type representing the schedule options.
+       */
+      schedule?: {
+        frequency?: TimeUnit;
+        timeout?: TimeUnit;
+        initialDelay?: TimeUnit;
+      };
+
+      
+    };
+  }
+
+type TimeUnit = { minutes?: number; hours?: number; seconds?: number }
