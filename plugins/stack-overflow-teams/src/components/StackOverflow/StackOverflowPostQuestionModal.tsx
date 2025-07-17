49569790c1eb655b7954b7d052d80cf78296cfe2
EstoesMoises <contactomoises1@gmail.com>
2025-07-17 17:17:51 +0100

    chore: importing following ESLINT guidance

--- plugins/stack-overflow-teams/src/components/StackOverflow/StackOverflowPostQuestionModal.tsx
+++ plugins/stack-overflow-teams/src/components/StackOverflow/StackOverflowPostQuestionModal.tsx
@@ -2,15 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useApi } from '@backstage/core-plugin-api';
 import Chip from '@material-ui/core/Chip'
 import { stackoverflowteamsApiRef } from '../../api';
-// eslint-disable-next-line no-restricted-imports
-import {
-  Modal,
-  Box,
-  TextField,
-  Button,
-  Typography,
-  Link,
-} from '@mui/material';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 import { useStackOverflowStyles } from './hooks';
 import { useNavigate } from 'react-router-dom';
 
