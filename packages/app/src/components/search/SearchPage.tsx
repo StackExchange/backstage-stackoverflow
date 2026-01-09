ebbd62ab350715376c966829d476ed5091d23f7a
EstoesMoises <contactomoises1@gmail.com>
2026-01-09 10:56:44 -0400

    feat: updated local backstage version, fix issues with collator plugin and UI issues for frontend plugin

--- packages/app/src/components/search/SearchPage.tsx
+++ packages/app/src/components/search/SearchPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { makeStyles, Theme, Grid, Paper, List } from '@material-ui/core';
 import { CatalogSearchResultListItem } from '@backstage/plugin-catalog';
 import {
